d7d0f0b9dabe5bf106c19cbe0588ee340ef194bc
shaiksajidhussain <sanjusazid0@gmail.com>
2025-09-22 02:12:01 +0530

    Added the base url

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,7 +1,7 @@
 const apiConfig = {
-    baseUrl: 'http://localhost:5001/api',
+    // baseUrl: 'http://localhost:5001/api',
             // or
-    // baseUrl: 'https://bhavani-chit-funds-backend.vercel.app/api',
+    baseUrl: 'https://bhavani-chit-funds-backend.vercel.app/api',
   
 }
 

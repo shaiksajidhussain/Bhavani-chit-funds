4cf25c3a70cc8d2696989e363556faf0c46a41db
shaiksajidhussain <sanjusazid0@gmail.com>
2025-10-03 11:08:58 +0530

    Added the urls in the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    host: '0.0.0.0',  // allows access from external IPs
+    port: 5173,       // optional, default is 5173
+    cors: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://bhavanichits.com',
+        'https://www.bhavanichits.com'
+      ]
+    }
+  }
 })
